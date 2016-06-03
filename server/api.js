6bcc538595387f0dc2dcf3725d1900c248b3abea
nooitaf <nooitaf@gmail.com>
2016-06-03 20:21:39 +0200

    add location check

--- server/api.js
+++ server/api.js
@@ -8,7 +8,7 @@ Router.route('/space/:name', function () {
   var res = this.response;
 	var query = String(this.params.name).trim()
 	var space = Spaces.findOne({'name': {$regex: query, $options: 'i'}})
-	if(space) {
+	if(space && space.data && space.data.location) {
 		res.end(JSON.stringify({status: 'success', space: space},null,2))
 	} else {
 		res.end(JSON.stringify({statusCode: 404, body: {status: 'fail', message: 'Space not found'}},null,2))
