b741e090c229b3ceae053c0991c804004a47281d
nooitaf <nooitaf@gmail.com>
2018-10-01 16:41:44 +0200

    make map data https (finally), as it seems to be supported now

--- client/client.js
+++ client/client.js
@@ -48,8 +48,8 @@ Template.space_map.rendered = function() {
   //   attribution: '© <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
   //   maxZoom: 18
   // }).addTo(map);
-  L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-  	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+  	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">CartoDB</a>',
   	subdomains: 'abcd',
   	maxZoom: 19
   }).addTo(map);
